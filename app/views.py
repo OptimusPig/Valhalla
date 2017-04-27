91ff1b4f3cc2de5e90cda943805d279dd0cc76c9
OptimusPig <limingxu215@gmail.com>
2017-04-27 11:11:21 +0900

    web form

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 import os
 import sqlite3
 from app import app
-from flask import Flask,render_template
+from flask import Flask,render_template,flash,redirect
 from .forms import LoginForm
 
 # app = Flask(__name__)
@@ -11,7 +11,7 @@ def login():
 	if form.validate_on_submit():
 		flash('Login requested for OpenID="' + form.openid.data +'",remember_me=' + str(form.remember_me.data))
 		return redirect("/index")
-	return render_template("login.html",title = "Sign In",form = form)
+	return render_template("login.html",title = "Sign In",form = form,providers = app.config["OPENID_PROVIDERS"])
 
 
 
