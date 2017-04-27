c4686b43af5d09297e2f05c53656ac71c6347bd7
OptimusPig <limingxu215@gmail.com>
2017-04-27 17:37:02 +0900

    small change

--- app/forms.py
+++ app/forms.py
@@ -3,5 +3,5 @@ from wtforms import StringField,BooleanField
 from wtforms.validators import DataRequired
 
 class LoginForm(Form):
-	openid = StringField("openid",validators=[DataRequired()])
+	openid = StringField("openid",validators=[DataRequired(message="please enter your openid")])
 	remember_me = BooleanField("remember_me",default=False)
